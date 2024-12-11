e1207b868937b270d9e74375671155bfbd49cc79
TomateAmargo <144570180+TomateAmargo@users.noreply.github.com>
2024-12-11 12:56:14 +0000

    Store, nao ta tudo feito como e obvio mas ta
    a

---
+++ wwwroot/BuySensors/store.js
@@ -0,0 +1,11 @@
+function showPopup() {
+    document.getElementById('popupOverlay').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+}
+
+function redirectToCheckout() {
+    window.location.href = "buying_page.html";
+}
