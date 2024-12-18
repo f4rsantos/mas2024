3dbaeaf64584351c68d5f9497b332214a5ebaaa1
TomateAmargo <144570180+TomateAmargo@users.noreply.github.com>
2024-12-18 00:09:12 +0000

    as

--- obj/Debug/net8.0/mas2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/mas2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mas2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d2e5f4cdf5d44c8c2661f092f11e1a6f5b5bf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f8efc493622c05fe28676d3e32fbe6c95e264d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("mas2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mas2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
